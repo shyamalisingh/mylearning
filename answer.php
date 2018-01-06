184bf710f284ffc9aabc053506d4ac8f21ff1ebe
shyamali <shyamali.singh.87@gmail.com>
2018-01-07 00:13:28 +0530

    my learning

---
+++ answer.php
@@ -0,0 +1,24 @@
+<html>
+<head></head>
+<body>
+<h1>Welcome</h1>
+
+<h2>Healthy Bite</h2>
+<div class="container">
+<div class="row">
+	<form action="newpassword.php" method="POST" class="col-md-4">
+	<div class="input-append">
+		<label class="control-label"><h1>Questions:</h1></label>
+		<select>
+		<option value="volvo">1:your fev actor name?</option>
+		<option value="saab">2:your first pet name?</option>
+		<option value="mercedes">3:your childhood friend name?</option>
+		<option value="audi">4:your school name?</option>
+		</select>
+	<div class="row">
+	<div class="input-append">
+		<label class="control-label"><h1>type answer:</h1>
+		<input type="text" name="answers" placeholder="type your answer" id="answers" class="form-control"/>
+		<input type="submit" value="enter" class="btn black-btn col-md-4"/>
+</body>
+</html>
---
+++ login_submit.php
@@ -0,0 +1,34 @@
+<?php
+// Start the session
+session_start();
+?>
+
+<?php
+  $dbConnectLink= mysqli_connect("localhost" , "shammi" , "shammi" , "learning");
+	if($dbConnectLink->connect_error){
+		die("connection faild".$dbConnectLink->connect_error);
+	
+	}
+	$email= $_POST["Username"];
+	$password= $_POST["password"];
+	
+	$sql="select name,email,password from users where email='$email' and password='$password'";
+	//echo $sql;
+	$result=mysqli_query($dbConnectLink,$sql);
+		$rowcount=mysqli_num_rows($result);
+		if($rowcount==1){
+			$row=mysqli_fetch_row($result);
+			
+			$_session["email"]=$email;
+			echo "welcome user $row[0]";
+			echo "<a href='logout.php?p=index.php'>logout</a>";
+			echo "<br/><br />";
+			
+			$_session["name"]=$row[0];
+		}
+		else{
+					echo "invalid user";
+			}
+	
+  
+?>
